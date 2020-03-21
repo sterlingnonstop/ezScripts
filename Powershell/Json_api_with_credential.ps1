359ae743c1d244fffb187262b087850c4b902f92
Sterling <docsmartou@gmail.com>
2020-03-21 10:56:48 +0800

    update

--- Powershell/Json_api_with_credential.ps1
+++ Powershell/Json_api_with_credential.ps1
@@ -7,7 +7,7 @@ $user  = "myaccount"
 $pass  = "mypass"
 $url =  "https://api.test.me/api"
 
-$wc  =  New-ObjectSystem.Net.WebClient
+$wc  =  New-Object System.Net.WebClient
 $wc.Credentials  =  New-Object System.Net.NetworkCredential($user, $pass)
 $info = $wc.DownloadString($url )  |  ConvertFrom-Json
 $info
