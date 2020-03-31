776d9c2bbd765af66e609e114d8f124d9ce9e029
Sterling <docsmartou@gmail.com>
2020-03-30 20:33:42 -0700

    commit 20200331

---
+++ Ens_IIS_web_config_ps4.ps1
@@ -0,0 +1,39 @@
+<#
+===============================================================
+PowerShell Version: 4.0
+===============================================================
+#>
+
+#Encrypt a specific section of a web.config file
+function EncWcSection($regiis, $section, $webFilePath,$logfile){
+    $Parms ="-pef ""$section"" ""$webFilePath"" -prov ""RsaProtectedConfigurationProvider"" "
+    $Prms = $Parms.Split(" ")
+	Write-Output "-----------------------" "$webFilePath:::$section" | out-File "$logfile" -Append
+    & "$regiis" $Prms 2>&1 | out-File "$logfile" -Append
+}
+
+#Path of aspnet_regiis.exe (.NET 4)
+$regiis = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe"
+#Target section names
+$sections = @("connectionStrings","appSettings","sessionState")
+#Output log
+$logfile = "iisenc_"+$(Get-Date).AddDays(-5).ToString("yyyyMMddHHmmss")+".log"
+#Encrypt All Web.config of IIS Sites
+Get-WebSite | ForEach-Object {
+	$webfilePath = $_.PhysicalPath 
+	if (Test-Path -path "$webFilePath\web.config"){
+        $sections | ForEach-Object {
+            EncWcSection $regiis $_ $webFilePath $logfile
+        }
+	}
+	
+	Get-WebApplication -Site "$($_.Name)" | ForEach-Object {
+    	$webFilePath = $_.PhysicalPath
+		if (Test-Path -path "$($webFilePath)\web.config"){
+	        $sections | ForEach-Object {
+    	         EncWcSection $regiis $_ $webFilePath $logfile
+        	}
+		}
+    }
+}
+Write-Output "--Finished--"
