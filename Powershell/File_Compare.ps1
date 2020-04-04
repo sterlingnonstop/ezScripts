502de432669c0230315522fbd9ba89ca1f13508c
Sterling <docsmartou@gmail.com>
2020-04-04 17:23:58 +0800

    Commit 20200404

--- Powershell/File_Compare.ps1
+++ Powershell/File_Compare.ps1
@@ -8,7 +8,12 @@ $refFolder = Get-ChildItem "D:\_tmp\Compare\a"
 $difFolder = Get-ChildItem "D:\_tmp\Compare\b"
 
 #Compare File Names
-Compare-Object $refFolder $difFolder -IncludeEqual
+$allResult = Compare-Object $refFolder $difFolder -IncludeEqual
 
 #Find equal file names
 #Compare-Object $refFolder $difFolder -IncludeEqual | Where {$_.SideIndicator -eq "=="}
+
+$allResult | Where {$_.SideIndicator -eq "=="} | ForEach-Object {
+    #$_.InputObject | Format-List * -Force
+    $_.InputObject.FullName
+}
