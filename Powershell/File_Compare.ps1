dda064516135f21c3b580ba3c56d3a74a20be7bf
sterling <docsmartou@gmail.com>
2020-04-05 10:57:58 +0800

    Commit 20200405

--- Powershell/File_Compare.ps1
+++ Powershell/File_Compare.ps1
@@ -4,16 +4,17 @@ PowerShell Version: 5.1
 ===============================================================
 #>
 
-$refFolder = Get-ChildItem "D:\_tmp\Compare\a"
-$difFolder = Get-ChildItem "D:\_tmp\Compare\b"
+$refFolder = Get-ChildItem "D:\a\a"
+$difFolder = Get-ChildItem -Recurse "D:\a\b"
 
 #Compare File Names
-$allResult = Compare-Object $refFolder $difFolder -IncludeEqual
+if ( !($refFolder -eq $null) -and !($difFolder -eq $null)){
+    $allResult = Compare-Object $refFolder $difFolder -IncludeEqual
+    #Find equal file names
+    #Compare-Object $refFolder $difFolder -IncludeEqual | Where {$_.SideIndicator -eq "=="}
 
-#Find equal file names
-#Compare-Object $refFolder $difFolder -IncludeEqual | Where {$_.SideIndicator -eq "=="}
-
-$allResult | Where {$_.SideIndicator -eq "=="} | ForEach-Object {
-    #$_.InputObject | Format-List * -Force
-    $_.InputObject.FullName
+    $allResult | Where {$_.SideIndicator -eq "=="} | ForEach-Object {
+        $_.InputObject.FullName
+        $($difFolder | Where-Object Name -eq $_.InputObject.Name).FullName
+    }
 }
