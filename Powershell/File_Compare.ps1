3634193b4c56937e9fb2969761b70b1b8d29431d
sterling <docsmartou@gmail.com>
2020-04-05 20:35:36 +0800

    Commit 20200405

--- Powershell/File_Compare.ps1
+++ Powershell/File_Compare.ps1
@@ -1,20 +1,31 @@
-<#
+﻿<#
 ===============================================================
 PowerShell Version: 5.1
 ===============================================================
 #>
 
-$refFolder = Get-ChildItem "D:\a\a"
+$refFolder = Get-ChildItem -Recurse "D:\a\a"
 $difFolder = Get-ChildItem -Recurse "D:\a\b"
-
-#Compare File Names
+$TrashBin = "D:\a\_remove"
+#"---sec 1---"
 if ( !($refFolder -eq $null) -and !($difFolder -eq $null)){
-    $allResult = Compare-Object $refFolder $difFolder -IncludeEqual
+    #"---sec 2---"
+    $allResult = Compare-Object $refFolder $difFolder -IncludeEqual -ExcludeDifferent
+
     #Find equal file names
     #Compare-Object $refFolder $difFolder -IncludeEqual | Where {$_.SideIndicator -eq "=="}
-
-    $allResult | Where {$_.SideIndicator -eq "=="} | ForEach-Object {
-        $_.InputObject.FullName
-        $($difFolder | Where-Object Name -eq $_.InputObject.Name).FullName
+    
+    $allResult | ForEach-Object {
+        $fr = $_.InputObject.FullName
+        $fd = $($difFolder | Where-Object Name -eq $_.InputObject.Name).FullName
+        
+        $frh = (Get-FileHash $fr).Hash
+        $fdh = (Get-FileHash $fd).Hash
+        if ( $frh -eq $fdh ){
+            Write-Output 
+            Write-Output @("equal", $fr, $fd, $frh )
+            Move-Item $fr -Destination "$TrashBin" 
+        }
     }
 }
+#"---sec end---"
