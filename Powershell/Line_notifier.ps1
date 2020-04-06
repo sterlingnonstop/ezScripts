3c21234e02e3beb25418f6de59b029052d8c6bf5
Sterling <docsmartou@gmail.com>
2020-04-06 01:03:36 -0700

    Commit 20200406

---
+++ Powershell/Line_notifier.ps1
@@ -0,0 +1,27 @@
+﻿<#
+===============================================================
+PowerShell Version: 5.1
+===============================================================
+#>
+
+#Access Token
+$token = "tokenXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+$uri="https://notify-api.line.me/api/notify"  
+$headers = @{ Authorization=("Bearer " + $token) }  
+
+#Youer message 
+$body=@{
+    message="Send a message from powershell"
+} 
+#Run Request
+try { 
+	$response = Invoke-WebRequest -Uri $uri -Method POST -Headers $headers -Body $body 
+
+	if ($webRequest.StatusCode -eq 200) {
+		Write-Output "success"
+	}else{
+		Write-Output "fail!"
+	}
+}catch {
+      Write-Output "Error:$($_.Exception.Response.StatusCode.Value__)"
+}
