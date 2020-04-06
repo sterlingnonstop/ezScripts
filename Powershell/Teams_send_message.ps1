710c41d1bad6522958c11bea882b21b889753816
Sterling <docsmartou@gmail.com>
2020-04-06 01:54:10 -0700

    Commit 20200406

---
+++ Powershell/Teams_send_message.ps1
@@ -0,0 +1,22 @@
+﻿<#
+===============================================================
+PowerShell Version: 5.1
+===============================================================
+#>
+
+#By mail
+#Find out the channel email address from Teams application and send your message to it
+$to ="teamsChannelEmail@find.it"
+
+#Other parameters
+$parms = @{
+	SmtpServer = "smtp.test.me"
+	From = "your@mail.test.me"
+	To = $to
+	Subject = "Test Subject"
+	Body = "Test Message"
+	Encoding = "UTF8"
+}
+
+#Send it
+Send-MailMessage @parms
